935eaff453f0b40070f80c02872396c5a4b80839
Satinder Singh <agnosticoder@gmail.com>
2024-12-28 02:36:49 -0500

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Satinder Singh on 28/12/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
